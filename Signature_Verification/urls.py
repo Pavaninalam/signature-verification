dadd4197ca39be789626811afa5ef3b83fd6c7b7
triveninalam-collab <triveninalam@gmail.com>
2026-03-31 19:50:35 +0530

    frontend + backend connected

--- Signature_Verification/urls.py
+++ Signature_Verification/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import JsonResponse
-
-def home(request):
-    return JsonResponse({"message": "Backend is running ✅"})
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', home),  # 👈 ADD THIS
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
+
+    # 👇 React UI serve
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
