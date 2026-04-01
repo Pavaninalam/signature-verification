892dd2f35cabd7adce89561f7cc8edbd6f818a1f
triveninalam-collab <triveninalam@gmail.com>
2026-04-01 15:10:51 +0530

    fix admin view naming error

--- admins/urls.py
+++ admins/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.AdminLoginView, name='AdminLogin'),
-    path('home/', views.AdminHomeView, name='AdminHome'),
-
+    path('login/', views.admin_login_view, name='AdminLogin'),
+    path('home/', views.admin_home_view, name='AdminHome'),
     path('users/', views.viewUsers, name='viewUsers'),
-    path('home/userDetails/', views.viewUsers, name='userDetails'),  # ✅ ADD THIS
-
     path('activate/<int:user_id>/', views.activateUser, name='activateUser'),
     path('delete/<int:user_id>/', views.deleteUser, name='deleteUser'),
 ]
